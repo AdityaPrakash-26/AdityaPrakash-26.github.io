f02113368cd8914a723145be6d5a0b9bc62469ff
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-22 17:12:51 +0530

    UI improvements

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log("Hello there! Thank you for visiting my website. If you are looking for the source code, it can be found at: https://github.com/AdityaPrakash-26/AdityaPrakash-26.github.io");
+console.log("Feel free to report bugs and share your feedback. Hope to see you there! :D");
+
 // Global
 var win = window,
     doc = document;
